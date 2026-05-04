28d07e4c515487541a094263e385dd364f834783
Fede Garcia <84251530+MFedericoGarcia@users.noreply.github.com>
2026-05-04 13:17:50 -0300

    first-commit

---
+++ Plan/Api.swift
@@ -0,0 +1,12 @@
+//
+//  Api.swift
+//  Plan
+//
+//  Created by Fede Garcia on 04/05/2026.
+//
+
+import Foundation
+
+struct Endpoints {
+    static let user = "https://jsonplaceholder.typicode.com/users"
+}
---
+++ Plan/NetworkManager.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkManager.swift
+//  Plan
+//
+//  Created by Fede Garcia on 04/05/2026.
+//
+
+import Foundation
+
+struct NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {
+        
+    }
+    
+    func getUsers(for endpoint: String, completed: @escaping([User]?, String?) -> Void) {
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This is an unvalid request")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){ data, response, error in
+            if let _ = error {
+                completed(nil, "Unable to complete the request")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server")
+                return
+            }
+            
+            guard let data = data else {
+                completed(nil, "The data received from the server was invalid")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let products = try decoder.decode([User].self, from: data)
+                
+                completed(products, nil)
+                
+            } catch {
+                completed(nil, "The data received from the server was invalid")
+            }
+            
+            
+        }
+        task.resume()
+    }
+}
