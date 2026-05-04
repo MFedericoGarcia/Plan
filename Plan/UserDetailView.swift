de1c36a07522d6aaa53e8f577115dd5776d1a08f
Fede Garcia <84251530+MFedericoGarcia@users.noreply.github.com>
2026-05-04 13:42:45 -0300

    create-detail
    style for views, created detail view

---
+++ Plan/UserDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  UserDetailView.swift
+//  Plan
+//
+//  Created by Fede Garcia on 04/05/2026.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var user: User
+    var body: some View {
+        ZStack{
+            LinearGradient(colors: [.blue, .black] , startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack{
+                Text(user.name)
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.blue)
+                Text(user.email)
+                    .font(.title3)
+                    .foregroundStyle(.secondary)
+
+                Text(user.phone)
+                    .font(.title3.bold())
+                    .foregroundStyle(.secondary)
+
+                Text(user.website)
+                    .font(.title3.bold())
+                    .foregroundStyle(.secondary)
+
+            }
+        }
+        .navigationTitle("User: \(user.username) ")
+        .navigationBarTitleDisplayMode(.inline)
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    UserDetailView(user: User(id: 2, name: "Federico", username: "Fede", email: "jhagsgs@shdhds.com", address: Address(street: "casa", suite: "10", city: "SM", zipcode: "1663", geo: Geo(lat: "15", lng: "13")), phone: "12344556", website: "jorge.com", company: Company(name: "Apple", catchPhrase: "hola", bs: "no se que es esto")))
+}
