2a3d6181e6bc31c6bc08f9ca8ed1478c9394a9d8
Fede Garcia <84251530+MFedericoGarcia@users.noreply.github.com>
2026-05-04 12:47:21 -0300

    Initial commit

---
+++ Plan/PlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanApp.swift
+//  Plan
+//
+//  Created by Fede Garcia on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
